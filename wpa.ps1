307204ab50939c849f12d50e63887d5205e74c50
antnks <antnks@users.noreply.github.com>
2018-11-20 13:49:47 +0200

    Initial commit

---
+++ wpa.ps1
@@ -0,0 +1,19 @@
+﻿# Dump saved WPA passphrases
+#
+
+$out = netsh.exe wlan show profiles | Select-String "All"
+
+foreach ($line in $out)
+{
+    $arr = $line -split ':'
+    $network = $arr[1]
+
+    $cmd = "netsh.exe wlan show profiles name=" + $network.trim() + " key=clear"
+    
+    $password = Invoke-Expression $cmd | Select-String "Key Content"
+    
+    $arr = $password -split ':'
+    $result = $network + ":" + $arr[1]
+
+    echo $result
+}
